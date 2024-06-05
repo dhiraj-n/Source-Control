6df412aae7cb8f899f9da56da09f1e0d126c53c3
= <=>
2024-06-05 15:12:59 +0530

    added static data

---
+++ Data/dbo.User_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[User] ([id], [fname], [lname], [test], [addr], [favcolor]) VALUES (1, N'Chirag    ', N'Gaonkar   ', 100, N'Panaji    ', N'Red       ')
+INSERT INTO [dbo].[User] ([id], [fname], [lname], [test], [addr], [favcolor]) VALUES (2, N'Ritesh    ', N'Gupta     ', 90, N'Vasco     ', N'Yellow    ')
--- Tables/dbo.User.sql
+++ Tables/dbo.User.sql
@@ -1,10 +1,12 @@
 CREATE TABLE [dbo].[User]
 (
-[id] [bigint] NULL,
+[id] [bigint] NOT NULL,
 [fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [lname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [test] [int] NULL,
 [addr] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [favcolor] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([id]) ON [PRIMARY]
 GO
