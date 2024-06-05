3c7368089dc70cd296c6e9fdbe712e7d549e58df
dhiraj-n <dhiraj_naik@persistent.com>
2024-06-05 14:35:46 +0530

    Added new column!

--- Tables/dbo.User.sql
+++ Tables/dbo.User.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[User]
 [fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [lname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [test] [int] NULL,
-[addr] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[addr] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[new] [int] NULL
 ) ON [PRIMARY]
 GO
