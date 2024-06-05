e5b4a2374377bacc164615642ac819d8400a5aa1
= <=>
2024-06-05 15:36:36 +0530

    Test2

--- Tables/dbo.Users.sql
+++ Tables/dbo.Users.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[User]
+CREATE TABLE [dbo].[Users]
 (
 [id] [bigint] NOT NULL,
 [fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -8,5 +8,5 @@ CREATE TABLE [dbo].[User]
 [favcolor] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[User] ADD CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([id]) ON [PRIMARY]
+ALTER TABLE [dbo].[Users] ADD CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([id]) ON [PRIMARY]
 GO
